a1aff32aee7f4bc6786cd84ef4939956ee42b8c6
Jan Schrempp <jan.schrempp@mni.thm.de>
2021-11-30 00:09:40 +0100

    added firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,30 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {provideFirebaseApp, initializeApp} from '@angular/fire/app';
+import {
+  getFirestore,
+  provideFirestore
+} from '@angular/fire/firestore';
+import {environment} from "../environments/environment";
+import {getStorage, provideStorage} from "@angular/fire/storage";
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideFirestore(() => getFirestore()),
+    provideStorage(() => getStorage()),
     BrowserModule,
     BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAzFk7Ufg0VV0x573mUWA5ock50-aVyzAI",
+    authDomain: "myquiz-b07f5.firebaseapp.com",
+    projectId: "myquiz-b07f5",
+    storageBucket: "myquiz-b07f5.appspot.com",
+    messagingSenderId: "625136282279",
+    appId: "1:625136282279:web:26edc9036edc74d8ae4982",
+    measurementId: "G-MH4B6GHZJB"
+  }
 };
 
 /*
